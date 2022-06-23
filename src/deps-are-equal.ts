d686e1ead7861b2ecb782a53590d05af8af3ab2b
Peter Juras <me@peterjuras.com>
2022-06-23 19:51:06 +0200

    fix: :bug: Reset state when dependency is added
    Previously adding an additional dependency to the dependency array did not reset the state.
    Example:
    previousDeps = [1, 2]
    newDeps = [1, 2, 3]
    now correctly resets the state.
    
    Fixes depsAreEqual() flaw #131

--- src/deps-are-equal.ts
+++ src/deps-are-equal.ts
@@ -2,11 +2,8 @@ export function depsAreEqual(
   prevDeps: React.DependencyList,
   deps: React.DependencyList
 ): boolean {
-  for (let i = 0; i < prevDeps.length; i++) {
-    if (Object.is(prevDeps[i], deps[i])) {
-      continue;
-    }
-    return false;
-  }
-  return true;
+  return (
+    prevDeps.length === deps.length &&
+    deps.every((dep, index) => Object.is(dep, prevDeps[index]))
+  );
 }
