d11f69e4ceea1b3c074a2b89c123669613334024
Peter Juras <me@peterjuras.com>
2024-08-10 09:11:13 +0200

    chore: Update eslint to version 9.9.0

--- scripts/ensure-publish-path.js
+++ scripts/ensure-publish-path.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const path = require("path");
 
 const splitted = process.cwd().split(path.sep);
 
 if (splitted[splitted.length - 1] !== "build") {
   console.log(
-    "ERROR! You can only publish outside of the transpiled /build folder\n",
+    "ERROR! You can only publish outside of the transpiled /build folder\n"
   );
   process.exit(1);
 }
--- src/is-function.ts
+++ src/is-function.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export function isFunction<S>(input: S | Function): input is Function {
   return typeof input === "function";
 }
